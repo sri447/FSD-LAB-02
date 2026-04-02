c29de4d7e580feb38eb7c8a9d5e0f0837b4e22dc
sri447 <www.sri8867nath091@gmail.com>
2026-04-02 11:20:50 +0530

    FSD LAB 02

---
+++ my-react-lab/src/compenets/about.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'; 
+ // This is a Class Component 
+class About extends Component { 
+  render() { 
+    return ( 
+      <div style={{ padding: '20px', textAlign: 'center' }}> 
+        <h1>About Us</h1> 
+        <p>This component was created using ES6 Class 
+syntax.</p> 
+      </div> 
+    ); 
+  } 
+ 
+ 
+} 
+ export default About;
